80287532bc0dbe0c25a39bdcf267c4dbf0ec00bb
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-10-19 14:05:17 +0200

    init

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,14 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
+	vite: {
+		css: {
+			preprocessorOptions: {
+				scss: {
+					additionalData: "@import \"@/assets/styles/main.scss\";",
+				},
+			},
+		},
+	},
 });
