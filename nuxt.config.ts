d08c65c2a595320bc63a39cb73a848257c599b04
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-10-24 14:01:01 +0200

    Update nuxt + Introduce VueUse plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,7 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
 	devtools: { enabled: true },
+	modules: ["@vueuse/nuxt"],
 	css: ["@/assets/styles/reset.css", "@/assets/styles/1-base/_base-default.scss"],
 	vite: {
 		css: {
