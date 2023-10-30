7c4d704cd676e30e3d78e240e0f3e11c20b6e392
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-10-30 12:29:43 +0100

    chore: introduce new eslint rules

--- eslint.config.js
+++ eslint.config.js
@@ -6,5 +6,11 @@ export default antfu({
 		quotes: "double", // On force les double quotes
 		indent: "tab", // On force l'indentation à être des tabs
 	},
-	rules: { "style/semi": ["error", "always"] }, // On force les semi-colons à la fin des lignes dans les fichiers js/ts
+	rules: {
+		"style/semi": ["error", "always"], // On force les semi-colons à la fin des lignes dans les fichiers js/ts
+		"eqeqeq": ["error", "always"], // On force l'usage de === et !==
+		"no-await-in-loop": ["error"], // On désactive l'usage de `await` dans les boucles
+		"no-nested-ternary": ["error"], // Nested ternay interdit
+		"ts/consistent-type-definitions": ["error", "type"], // On oblige l'utilisation de `type` au lieu de `interface`
+	},
 });
