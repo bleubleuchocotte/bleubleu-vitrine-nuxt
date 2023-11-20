4926604051c48264618d8594d6757ea98cf8e0e3
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-11-20 17:42:42 +0100

    feat: set seo

--- app.vue
+++ app.vue
@@ -1,5 +1,54 @@
+<script setup lang="ts">
+const request = useRequestURL();
+
+// On place ici les propriétés réactives
+// S'assurer avant la mise en prod que useSeoMeta est vraiment nécessaire au profit de useServerSeoMeta
+useSeoMeta({
+	title: "Le titre du site",
+	ogTitle: "Le titre du site",
+
+	description: "La description du site.",
+	ogDescription: "La description du site.",
+
+	ogImage: `${request.origin}/images/og-image.png`,
+	ogUrl: request.origin,
+
+	twitterCard: "summary_large_image",
+});
+
+// On place ici les propriétés non-réactives
+useServerSeoMeta({
+	robots: "index, follow",
+	themeColor: "#FFFFFF",
+	creator: "BleuBleu Chocotte",
+});
+
+useHead({
+	htmlAttrs: {
+		lang: "en",
+	},
+	link: [
+		{
+			rel: "icon",
+			type: "image/png",
+			href: `${request.origin}/favicon-32x32.ico`,
+		},
+	],
+});
+</script>
+
 <template>
-	<div>
-		<NuxtWelcome />
+	<div class="starter">
+		<AppTypography>Ceci est le template</AppTypography>
 	</div>
 </template>
+
+<style scoped lang="scss">
+.starter {
+	height: 100vh;
+	width: 100vw;
+
+	color: #FFFFFF;
+	background-color: #000000;
+}
+</style>
