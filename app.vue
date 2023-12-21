d4c0c6cc3f50df35a52f6f4b6cc480bc029e52c0
Thomas <thomas.auffroy@gmail.com>
2023-12-21 17:05:38 +0100

    chore: update fake text

--- app.vue
+++ app.vue
@@ -39,7 +39,10 @@ useHead({
 
 <template>
 	<div class="starter">
-		<AppTypography>Ceci est le template</AppTypography>
+		<h1>Heading 1</h1>
+		<h2>Heading 2</h2>
+		<h3>Heading 3</h3>
+		<p>Un paragraphe: Lorem ipsum dolor sit amet consectetur adipisicing elit. Aliquid assumenda odio possimus praesentium tempora cumque quis aliquam obcaecati doloremque placeat, et aperiam autem impedit veritatis voluptatibus. Illum laborum nesciunt eaque.</p>
 	</div>
 </template>
 
