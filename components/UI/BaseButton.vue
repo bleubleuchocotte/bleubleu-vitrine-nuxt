83730da6dd2bee1a1f97cd9ff38edb3d69c7f3ce
Thomas <thomas.auffroy@gmail.com>
2023-12-22 11:11:18 +0100

    feat: introduce basic button component

---
+++ components/UI/BaseButton.vue
@@ -0,0 +1,48 @@
+<script setup lang="ts">
+type ButtonHTMLAttributes = {
+	autofocus?: boolean
+	disabled?: boolean
+	form?: string
+	formaction?: string
+	formenctype?: string
+	formmethod?: string
+	formnovalidate?: boolean
+	formtarget?: string
+	name?: string
+	type?: "submit" | "reset" | "button"
+	value?: string | ReadonlyArray<string> | number
+};
+
+withDefaults(defineProps<ButtonHTMLAttributes>(), {
+	type: "button",
+	disabled: false,
+});
+</script>
+
+<template>
+	<button class="button" :disabled="disabled" :type="type">
+		<slot />
+	</button>
+</template>
+
+<style scoped lang="scss">
+.button {
+	&:not([disabled]) {
+		cursor: pointer;
+
+		&:hover {
+			color: red;
+		}
+
+		&:active {
+			color: green;
+		}
+
+		// focus-visible let the UA determines when it need to apply focus style
+		&:focus,
+		&:focus-within {
+			outline: 1px solid currentColor;
+		}
+	}
+}
+</style>
