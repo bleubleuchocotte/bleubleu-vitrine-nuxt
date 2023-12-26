75c25016fe05aa11de26f6105bf8fbce725951a0
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-12-26 11:29:41 +0100

    feat: create footer component

---
+++ components/TheFooter.vue
@@ -0,0 +1,16 @@
+<template>
+	<footer class="footer">
+		Ceci est le footer
+	</footer>
+</template>
+
+<style scoped lang="scss">
+.footer {
+	height: 30px;
+	width: 100%;
+
+	text-align: center;
+	background-color: rgba(#000000, 0.8);
+	color: rgba(#ffffff, 0.8);
+}
+</style>
