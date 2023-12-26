173102f1fc9172bb3df21e0bf24dfd3c95b6f9bf
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-12-26 12:41:00 +0100

    feat: update error component

--- components/TheFatalError.vue
+++ components/TheFatalError.vue
@@ -14,16 +14,22 @@ defineProps<ComponentProps>();
 </script>
 
 <template>
-	<div>
-		<h1>Oops !</h1>
-		<h2>{{ error.statusCode }}</h2>
+	<div class="fatal-error">
+		<h1>{{ error.statusCode }}</h1>
 		<p>
 			{{ error.message }}
-			<code>
-				{{ error.stack }}
-			</code>
 		</p>
-
-		<h3>Contactez un admnistrateur pour plus d'informations</h3>
+		<pre class="fatal-error__stack" v-html="error.stack" />
 	</div>
 </template>
+
+<style scoped lang="scss">
+.fatal-error {
+	&__stack {
+		white-space: pre-line;
+		overflow: hidden;
+
+		@include padding();
+	}
+}
+</style>
