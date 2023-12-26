3490ef40ffb289b0cf39daf53f343d92a1e2f8e5
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-12-26 11:29:50 +0100

    feat: create header component

---
+++ components/TheHeader.vue
@@ -0,0 +1,16 @@
+<template>
+	<header class="header">
+		Ceci est le header
+	</header>
+</template>
+
+<style scoped lang="scss">
+.header {
+	height: 30px;
+	width: 100%;
+
+	text-align: center;
+	background-color: rgba(#000000, 0.8);
+	color: rgba(#ffffff, 0.8);
+}
+</style>
