9e268f05893927f048d6d890e87b7bc36f716594
Thomas Auffroy <thomas.auffroy@gmail.com>
2023-12-29 10:15:16 +0100

    feat: introduce loading indicator

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,4 +1,12 @@
+<script setup lang="ts">
+const { start } = useLoadingIndicator({
+	throttle: 0,
+});
+start();
+</script>
+
 <template>
+	<NuxtLoadingIndicator />
 	<div>
 		<TheHeader />
 		<slot />
