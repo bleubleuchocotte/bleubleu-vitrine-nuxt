51afa4b78f7785583a8d44267dc74df6c469a781
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-16 11:20:05 +0100

    feat(composables): create math.ts for math composable

--- composables/utils.ts
+++ composables/utils.ts
@@ -3,9 +3,3 @@ export function useUid() {
 	const randomPart = Math.random().toString(36).substring(2, 8); // 6 random characters
 	return `${timestamp}-${randomPart}`;
 }
-
-export function useRandomInt(min: number, max: number) {
-	const _min = Math.ceil(min);
-	const _max = Math.floor(max);
-	return Math.floor(Math.random() * (_max - _min) + _min); // The maximum is exclusive and the minimum is inclusive
-}
