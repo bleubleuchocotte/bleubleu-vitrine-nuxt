0abc6471ae57130416c25b342717e2d1c7e68665
Thomas Auffroy <thomas.auffroy@gmail.com>
2024-01-18 16:49:26 +0100

    feat(icons): introduce icons

---
+++ components/Icon/IconLinkArrowRight.vue
@@ -0,0 +1,5 @@
+<template>
+	<svg width="15" height="16" viewBox="0 0 15 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+		<path d="M0 7.99219H14M14 7.99219L6.41667 0.492188M14 7.99219L6.41667 15.4922" stroke="currentColor" />
+	</svg>
+</template>
